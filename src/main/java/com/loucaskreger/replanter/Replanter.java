e2867fd1969dc5235e90035ec57b514cc55b8d33
Tilwa Qendov <tilwa.qendov@gmail.com>
2022-09-09 22:46:17 +0200

    Use random ticks instead of outline to determine maturity for warts & cocoa

--- src/main/java/com/loucaskreger/replanter/Replanter.java
+++ src/main/java/com/loucaskreger/replanter/Replanter.java
@@ -6,16 +6,15 @@ import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class Replanter implements ModInitializer {
-
     private static final MinecraftClient mc = MinecraftClient.getInstance();
 
     @Override
@@ -23,45 +22,42 @@ public class Replanter implements ModInitializer {
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 BlockPos pos = hitResult.getBlockPos();
-                ItemStack heldStack = player.getMainHandStack();
-                ItemStack offhandStack = player.getOffHandStack();
+                ItemStack heldStack = player.getStackInHand(hand);
 
-                if (hand == Hand.MAIN_HAND && !heldStack.isEmpty()) {
-                    this.replaceCrop(heldStack, pos, world);
-                } else if (hand == Hand.OFF_HAND && !offhandStack.isEmpty()) {
-                    this.replaceCrop(offhandStack, pos, world);
+                if (!heldStack.isEmpty()) {
+                    this.replaceCrop(heldStack.getItem(), pos, world);
                 }
             }
+            // Let the game handle the right-click to plant the crop
             return ActionResult.PASS;
         });
     }
 
-    private void replaceCrop(ItemStack stack, BlockPos pos, World world) {
+    private void replaceCrop(Item heldItem, BlockPos pos, World world) {
         BlockState state = world.getBlockState(pos);
         Block block = state.getBlock();
         ClientPlayerInteractionManager im = mc.interactionManager;
-        if (stack.getItem() instanceof BlockItem) {
-            Block itemBlock = ((BlockItem) stack.getItem()).getBlock();
+        if (im == null) return;
 
-            if (block instanceof CropBlock) {
+        if (heldItem instanceof BlockItem heldBlockItem) {
+            Block heldBlock = heldBlockItem.getBlock();
 
-                if (itemBlock instanceof CropBlock) {
-                    if (((CropBlock) block).isMature(state)) {
-                        im.attackBlock(pos, Direction.DOWN);
-                    }
-                }
-
-                // Exception for nether warts
-                // If nether wart block outline shape y value is max size then the block is
-            } else if (block instanceof NetherWartBlock wartBlock && block == itemBlock) {
-                // fully grown.
-                if ((wartBlock.getOutlineShape(state, world, pos, ShapeContext.absent()).getBoundingBox().getYLength()
-                        * 16) == 14) {
+            if (block instanceof CropBlock cropBlock) {
+                if (cropBlock.isMature(state) && heldBlock instanceof CropBlock) {
                     im.attackBlock(pos, Direction.DOWN);
                 }
+
+            } else if (block instanceof NetherWartBlock wartBlock) {
+                // Netherwarts don't tick anymore when they're fully grown
+                if (!wartBlock.hasRandomTicks(state) && heldBlock instanceof NetherWartBlock) {
+                    im.attackBlock(pos, Direction.DOWN);
+                }
+
             } else if (block instanceof CocoaBlock cocoaBlock) {
-                if ((cocoaBlock.getOutlineShape(state, world, pos, ShapeContext.absent()).getBoundingBox().getYLength() == 0.5625)) {
-                    im.attackBlock(pos, world.getBlockState(pos).get(CocoaBlock.FACING));
+                // Cocoa beans don't tick anymore when they're fully grown
+                if (!cocoaBlock.hasRandomTicks(state) && heldBlock instanceof CocoaBlock) {
+                    // NOTE In survival mode, the cocoa pod only breaks when using an iron axe or better
+                    im.attackBlock(pos, state.get(CocoaBlock.FACING));
                 }
             }
         }
